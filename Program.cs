98497d4ba82fadea327d392f6d480502b8724d70
BogRyadom <divine.bohu@gmail.com>
2023-01-12 22:25:35 +0200

    Создал метод для возведения числа в степень. и почитстил код

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace git2
+﻿namespace git2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
         }
+        static long Power(long x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n % 2 == 0)
+            {
+                var p = Power(x, n / 2);
+                return p * p;
+            }
+            else
+            {
+                return x * Power(x, n - 1);
+            }
+        }
+
+
+
     }
 }
+
+    
+
